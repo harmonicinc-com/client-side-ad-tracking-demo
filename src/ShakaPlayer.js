ff88f39441b3eb5791cf7cfacce49ea163d17124
Albert Cheung <albert.cheung@harmonicinc.com>
2020-11-18 01:07:04 +0800

    init shaka player imperatively

--- src/ShakaPlayer.js
+++ src/ShakaPlayer.js
@@ -3,8 +3,9 @@ import "shaka-player/dist/controls.css";
 import shaka from "shaka-player/dist/shaka-player.ui.js";
 import muxjs from 'mux.js';
 
-const initPlayer = async (pVideoRef, props) => {
-    const ui = pVideoRef["ui"];
+const initPlayer = async (pVideoRef, containerRef, props) => {
+    const player = new shaka.Player(pVideoRef);
+    const ui = new shaka.ui.Overlay(player, containerRef, pVideoRef);
     const config = {
       controlPanelElements: [
         "rewind",
@@ -18,8 +19,9 @@ const initPlayer = async (pVideoRef, props) => {
       ]
     };
     ui.configure(config);
+    ui.getControls();
     const controls = ui.getControls();
-    const player = controls.getPlayer();
+
     player.configure('manifest.defaultPresentationDelay', 12.0 /* seconds */);
     player.configure('manifest.dash.ignoreSuggestedPresentationDelay', true);
     // player.configure('manifest.availabilityWindowOverride', 105.0);
@@ -47,21 +49,21 @@ const onError = (event: any) =>
   console.error("Error code", event);
 
 function ShakaPlayer(props) {
+  const containerRef = React.createRef();
   const videoRef = React.createRef();
 
   React.useEffect(() => {
     window.muxjs = muxjs;
-    document.addEventListener("shaka-ui-loaded", () => {
-      initPlayer(videoRef.current, props);
-    });
-    initPlayer(videoRef.current, props);
+    // document.addEventListener("shaka-ui-loaded", () => {
+    //   initPlayer(videoRef.current, props);
+    // });
+    initPlayer(videoRef.current, containerRef.current, props);
   }, []);
 
   return (
     <div>
         <div
-            data-shaka-player-container
-            data-shaka-player-cast-receiver-id="7B25EC44"
+            ref={containerRef}
             style={{ maxWidth: props.width }}>
             <video
                 data-shaka-player
