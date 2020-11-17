aad7aae437dc158ba17363063a153d44b040ba6c
Albert Cheung <albert.cheung@harmonicinc.com>
2020-11-18 01:51:43 +0800

    nested list for ad pod

--- src/AdPodList.js
+++ src/AdPodList.js
@@ -3,21 +3,50 @@ import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import MovieIcon from '@material-ui/icons/Movie';
+import FolderIcon from '@material-ui/icons/Folder';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import HourglassFullIcon from '@material-ui/icons/HourglassFull';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 import { SessionContext } from './SessionService';
 import './AdPodList.css'
 
 const useStyles = makeStyles((theme) => ({
-  adItem: {
+  itemText: {
     fontSize: 13,
     // whiteSpace: "nowrap",
     // overflow: "hidden",
     // textOverflow: "ellipsis"
   },
-  podOnAir: {
+  podItem: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(0)
+  },
+  podItemOnAir: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(0),
     backgroundColor: "#368cee"
   },
-  adOnAir: {
+  adItem: {
+    paddingLeft: theme.spacing(4),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(0)
+  },
+  adItemOnAir: {
+    paddingLeft: theme.spacing(4),
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(0),
     backgroundColor: "#71B4FF"
+  },
+  trackingUrlItem: {
+    paddingLeft: theme.spacing(8),
+    paddingTop: theme.spacing(0),
+    paddingBottom: theme.spacing(1)
+  },
+  greenIcon: {
+    color: 'green',
   }
 }));
 
@@ -35,50 +64,65 @@ function AdPodList() {
       {pods ?
         <List>
           {pods.map((pod) =>
-            <ListItem key={pod.id} className={pod.start < playheadInMs && playheadInMs < pod.start + pod.duration ? classes.podOnAir : ''}>
-              <ListItemText disableTypography className={classes.adItem}>
-                <div>
-                  Ad Pod: {pod.id}
-                </div>
-                <div>
-                  Time: {(pod.start / 1000).toFixed(1)}s, Duration: {(pod.duration / 1000).toFixed(1)}s
-                </div>
-                <List>
-                  {pod.ads.map((ad) =>
-                    <ListItem key={ad.id} className={ad.start < playheadInMs && playheadInMs < ad.start + ad.duration ? classes.adOnAir : ''}>
-                      <ListItemText disableTypography className={classes.adItem}>
+            <>
+              <ListItem key={pod.id} className={pod.start < playheadInMs && playheadInMs < pod.start + pod.duration ? classes.podItemOnAir : classes.podItem}>
+                <ListItemIcon>
+                  <FolderIcon />
+                </ListItemIcon>
+                <ListItemText disableTypography className={classes.itemText}>
+                  <div>
+                    Ad Pod: {pod.id}
+                  </div>
+                  <div>
+                    Time: {(pod.start / 1000).toFixed(1)}s, Duration: {(pod.duration / 1000).toFixed(1)}s
+                  </div>
+                </ListItemText>
+              </ListItem>
+              <List>
+                {pod.ads.map((ad) =>
+                  <>
+                    <ListItem key={ad.id} className={ad.start < playheadInMs && playheadInMs < ad.start + ad.duration ? classes.adItemOnAir : classes.adItem}>
+                      <ListItemIcon>
+                        <MovieIcon />
+                      </ListItemIcon>
+                      <ListItemText disableTypography className={classes.itemText}>
                         <div>
                           Ad: {ad.id}
                         </div>
                         <div>
                           Time: {(ad.start / 1000).toFixed(1)}s, Duration: {(ad.duration / 1000).toFixed(1)}s
                         </div>
-                        {ad.trackingUrls ? 
-                          <List variant="flush">
-                            {ad.trackingUrls.map((trackingUrl,index) =>
-                              <ListItem key={index}>
-                                <ListItemText disableTypography className={classes.adItem}>
-                                  <div>
-                                    Event: {trackingUrl.event}
-                                  </div>
-                                  <div>
-                                    URL: {trackingUrl.url}
-                                  </div>
-                                  <div>
-                                    Sent: {trackingUrl.sent ? 'Yes' : ''}
-                                  </div>
-                                </ListItemText>
-                              </ListItem>
-                            )}
-                          </List>
-                          : null
-                        }
                       </ListItemText>
                     </ListItem>
-                  )}
-                </List>
-              </ListItemText>
-            </ListItem>
+                    {ad.trackingUrls ? 
+                      <List>
+                        {ad.trackingUrls.map((trackingUrl,index) =>
+                          <ListItem key={index} className={classes.trackingUrlItem}>
+                            <ListItemIcon>
+                              {trackingUrl.sent ? <CheckCircleIcon className={classes.greenIcon} /> : <RadioButtonUncheckedIcon />}
+                            </ListItemIcon>
+                            <ListItemText disableTypography className={classes.itemText}>
+                              <div>
+                                Event: {trackingUrl.event}
+                              </div>
+                              <div>
+                                URL: {trackingUrl.url}
+                              </div>
+                              {trackingUrl.startTime ?
+                                <div>
+                                  Time: {trackingUrl.startTime}
+                                </div>
+                                : null}
+                            </ListItemText>
+                          </ListItem>
+                        )}
+                      </List>
+                      : null
+                    }
+                  </>
+                )}
+              </List>
+            </>
           )}
         </List>
         : null
