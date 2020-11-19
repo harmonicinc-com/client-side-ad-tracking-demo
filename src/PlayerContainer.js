20bc8cfb7ca5151a4e4bc90d69551271b75187f9
Albert Cheung <albert.cheung@harmonicinc.com>
2020-11-19 15:35:21 +0800

    show time to next ad break

--- src/PlayerContainer.js
+++ src/PlayerContainer.js
@@ -17,14 +17,22 @@ function PlayerContainer() {
         session.setPlaybackPaused();
     };
 
+    const playhead = session.presentationStartTime + session.currentTime * 1000;
+
+    const timeToNextBreak = Math.min(Infinity,
+        ...session.adPods.filter(p => p.start > playhead).map(p => p.start)) - playhead;
+
     return (
         <div>
+            <ShakaPlayer key={"player-" + session.localSessionId} src={session.manifestUrl} onTimeUpdate={updateTime} onPlaying={startTracker} onPaused={stopTracker} />
             <div>
-                <ShakaPlayer key={"player-" + session.localSessionId} src={session.manifestUrl} onTimeUpdate={updateTime} onPlaying={startTracker} onPaused={stopTracker} />
                 Raw currentTime from video element: {session.currentTime.toFixed(1)}s
             </div>
             <div>
-                Playhead date time: {new Date(session.presentationStartTime + session.currentTime * 1000).toLocaleString()}
+                Playhead date time: {new Date(playhead).toLocaleString()}
+            </div>
+            <div>
+                Time to next ad break: {timeToNextBreak !== Infinity ? Math.ceil(timeToNextBreak/1000).toFixed(0) + 's' : '-'}
             </div>
         </div>
     );
